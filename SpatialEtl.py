c859a39460bfa5d40eaf14a304ad096d2508c019
Katie-McDaniel888 <125925366+Katie-McDaniel888@users.noreply.github.com>
2023-05-04 17:04:01 -0600

    Final_Project_McDaniel

---
+++ SpatialEtl.py
@@ -0,0 +1,25 @@
+#Katie McDaniel
+#Final - GIS 305
+#Spring 2023
+
+class SpatialEtl:
+    def __init__(self, config_dict):
+        self.config_dict = config_dict
+
+    def extract(self):
+        """
+        Extracts addresses from google form (avoid point layer) and puts them in the project directory (yaml file)
+        """
+        print(f"Extracting data from {self.config_dict.get('remote_url')}" f" to {self.config_dict.get('proj_dir')}")
+
+    def transform(self):
+        """
+        Transforms the addresses from google form using a geocoder into a readablel XY Table for ArcGIS Pro.
+        """
+        print(f"Transforming {self.config_dict.get('proj_dir')}")
+
+    def load(self):
+        """
+        Loads the final avoid point layer into the main function for analysis.
+        """
+        print(f"Loading data into {self.config_dict.get('proj_dir')}")
